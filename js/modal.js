76766a314cc366b0cc3613fc26547d41e56c081b
Martin <martin.p.nikolov@gmail.com>
2017-03-20 00:32:41 +0200

    content completed on the site. About me, Contacts, Portfolio - done. Jquery for modal window in portfolio gallery.

---
+++ js/modal.js
@@ -0,0 +1,29 @@
+var modal = document.getElementById('myModal');
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById('modal-image');
+
+function openModal() {
+    modal.style.display = "block";
+    modalImg.src = this.getAttribute("data-highres");
+}
+
+/*img.onclick = function() {
+ modal.style.display = "block";
+ modalImg.src = this.getAttribute("data-highres");
+ }*/
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function() {
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+window.openModal = function(img) {
+    modal.style.display = "block";
+    modalImg.src = img.getAttribute("data-highres");
+}
